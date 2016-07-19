e4f9200a0d44287297976dfadabf37b72451a98a
Mariusz <mpostol@cas.eu>
2016-07-19 15:01:40 +0000

    CommServer.DA.Viewer maintenance stand alone release - moving common project to the CommServer folder. Next step.

---
+++ DeviceSimulatorTests/DeviceSimulatorUnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CAS.Lib.DeviceSimulator.Tests
+{
+  [TestClass]
+  public class DeviceSimulatorUnitTest1
+  {
+    [TestMethod]
+    public void DeviceSimulatorTestMethod1()
+    {
+      Device _device = new Device();
+      Assert.IsNotNull(_device);
+    }
+  }
+}
