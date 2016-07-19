45ba1cd35845ad5717300b073dff392a3fdabfe6
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-07-19 20:09:01 +0000

    CAS.CommServer.DA.Client.Configuration update - moving project to new desalination

---
+++ VS/trunk/CommServer/CommServer.DAClientConfiguration.UnitTests/ConfigurationUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CAS.DataPorter.Configurator;
+
+namespace CAS.CommServer.DA.Client.Configuration.UnitTests
+{
+  [TestClass]
+  public class DataPorterConfigLibUnitTest
+  {
+    [TestMethod]
+    public void ConfigurationManagementCreatorMethod1()
+    {
+      bool _disposedRaised = false;
+      using (ConfigurationManagement _newConfiguration = new ConfigurationManagement())
+      {
+        _newConfiguration.Disposed += (x, y) => _disposedRaised = true; 
+        Assert.IsNotNull(_newConfiguration.Configuartion);
+        Assert.AreEqual<string>("OPCViewerSession", _newConfiguration.DefaultFileName);
+        Assert.IsNotNull(_newConfiguration.Menu);
+        Assert.IsNotNull(_newConfiguration.OpenFileDialog);
+        Assert.IsFalse(_disposedRaised);
+      }
+      Assert.IsTrue(_disposedRaised);
+    }
+  }
+}
