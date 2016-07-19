4b7dbe64c016a58af2af4c8159a26603b5707b91
Mariusz <mpostol@cas.eu>
2016-07-19 18:58:07 +0000

    PR37-CAS_LIB_PCKG - update - moved to new location, added dependent packages, compiled against 4.51. Issue #: 4846

--- CommServer.DeviceSymulator/Properties/AssemblyInfo.cs
+++ CommServer.DeviceSymulator/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿//<summary>
-//  Title   : Assembly info for: Device Simulator
+//  Title   : Assembly info for: CAS.CommServer.DeviceSimulator
 //  System  : Microsoft Visual C# .NET
 //  $LastChangedDate$
 //  $Rev$
@@ -7,7 +7,7 @@
 //  $URL$
 //  $Id$
 //
-//  Copyright (c) 2000-2015 CAS LODZ POLAND
+//  Copyright (c) 2000-2016 CAS LODZ POLAND
 //  +48 (42) 686 25 47
 //  techsupp@cas.eu
 //  www.cas.eu
@@ -16,18 +16,16 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Device Simulator")]
-[assembly: AssemblyDescription("CAS CommServer Family: Device Simulator")]
+[assembly: AssemblyTitle("CAS.CommServer.DeviceSimulator")]
+[assembly: AssemblyDescription("Device Simulator for CommServer family.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CAS")]
 [assembly: AssemblyProduct("CAS CommServer Family")]
-[assembly: AssemblyCopyright("Copyright (c) 2000-2015 CAS LODZ POLAND")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
 [assembly: AssemblyTrademark("CommServer")]
 [assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("D23A1A53-1461-4a5c-9BAF-35CCB7111723")]
-[assembly: AssemblyVersion("5.00.00.*")]
-[assembly: AssemblyFileVersion("5.00.00")]
+[assembly: AssemblyVersion("5.01.00.*")]
+[assembly: AssemblyFileVersion("5.01.00")]
 
