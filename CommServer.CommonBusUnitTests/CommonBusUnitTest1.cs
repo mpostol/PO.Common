6919ba8242026c1a195d264ada17516da9edd0bf
Mariusz <mpostol@cas.eu>
2016-08-16 19:43:49 +0000

    CommServer.DA.Server - remove direct dependencies - Refactoring CommonBus - moving to the CommServer folder.
    Issue #: 4859

---
+++ CommServer.CommonBusUnitTests/CommonBusUnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CAS.Lib.CommonBus.Tests
+{
+  [TestClass]
+  public class CommonBusUnitTest1
+  {
+    [TestMethod]
+    public void CommonBusUnitTestMethod1()
+    {
+      DataProviderID _provider = new ConcreteDataProviderID();
+      Assert.IsNotNull(_provider);
+      Assert.AreEqual<int>(5, _provider.GetDataProviderDescription.Version.Major);
+    }
+  }
+  internal class ConcreteDataProviderID : DataProviderID
+  {
+
+    protected override void WriteSettings(System.Xml.XmlWriter pSettings)
+    {
+      throw new NotImplementedException();
+    }
+
+    protected override void ReadSettings(System.Xml.XmlReader pSettings)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override ApplicationLayer.IApplicationLayerMaster GetApplicationLayerMaster(CAS.Lib.RTLib.Management.IProtocolParent pStatistic, CommonBusControl pParent)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override IAddressSpaceDescriptor[] GetAvailiableAddressspaces()
+    {
+      throw new NotImplementedException();
+    }
+
+    public override IItemDefaultSettings GetItemDefaultSettings(short AddressSpaceIdentifier, ulong AddressInTheAddressSpace)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ CommServer.CommonBusUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CommonBusTests")]
+[assembly: AssemblyDescription("CommonBusTests")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("CommServer")]
+[assembly: AssemblyCopyright("Copyright ©  CAS 2014")]
+[assembly: AssemblyTrademark("CommServer")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d7e134e8-6bd7-4250-9722-1991ce44cd3d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("5.0.0.*")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
