1459205796ddd07dc1527dd1f89024ca3f2b3f74
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-10-12 20:13:58 +0000

    CommServer.UA.Viewer - release - working on CommServer, moving CommServer.DAServerConfiguration Issue #: 4867

---
+++ VS/trunk/CommServer/CommServer.DAServeConfiguration/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿//<summary>
+//  Title   : Assembly info for: CAS.CommServer.DAServerConfiguration
+//  System  : Microsoft Visual C# .NET
+//  $LastChangedDate: $
+//  $Rev:  $
+//  $LastChangedBy:  $
+//  $URL:  $
+//  $Id:  $
+//
+//  Copyright (c) 2000-2016 CAS LODZ POLAND
+//  +48 (42) 686 25 47
+//  techsupp@cas.eu
+//  www.cas.eu
+//</summary>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CAS.CommServer.DAServerConfiguration")]
+[assembly: AssemblyDescription("CAS.CommServer.DAServerConfiguration - CommServer Configuration Management")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("CAS CommServer Family")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("CommServer")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("7c0ea31e-34e9-42bb-8751-785b2f85b3cf")]
+[assembly: AssemblyVersion("4.1.2.*")]
+[assembly: AssemblyFileVersion("4.1.2")]
+
+
