9ef48638b6827953638a432b6978bd2090f31aa7
Mariusz <mpostol@cas.eu>
2016-10-25 11:07:29 +0000

    CommServer - new release - new release 5.01.05 Issue #: 4877

--- CommServer.DeviceSimulatorTests/Properties/AssemblyInfo.cs
+++ CommServer.DeviceSimulatorTests/Properties/AssemblyInfo.cs
@@ -26,6 +26,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a3446e1f-680a-4f9c-afa7-b8fa01124e7d")]
-[assembly: AssemblyVersion("5.01.04.*")]
-[assembly: AssemblyFileVersion("5.01.04")]
+[assembly: AssemblyVersion("5.01.05.*")]
+[assembly: AssemblyFileVersion("5.01.05")]
 
--- CommServer.ScriptsUnitTest/CreateTag.cmd
+++ CommServer.ScriptsUnitTest/CreateTag.cmd
@@ -6,8 +6,8 @@ rem//  $URL$
 rem//  $Id$
 
 
-svn mkdir svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.rel_5_01_04  -m "created new svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.rel_5_01_04"
-svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/CommServer/ svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.rel_5_01_04 -m "created copy svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.rel_5_01_04"
+svn mkdir svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.rel_5_01_05  -m "created new svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.rel_5_01_05"
+svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/CommServer/ svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.rel_5_01_05 -m "created copy svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.rel_5_01_05"
 
 pause ....
 
