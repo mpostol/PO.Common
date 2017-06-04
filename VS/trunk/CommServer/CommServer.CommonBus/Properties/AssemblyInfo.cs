584f1b00a634e5a8999bcb5afffb721736a2fbc9
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2017-06-04 20:11:25 +0000

    CAS.CommServer.ProtocolHub - refactoring CommServer folder - updated AssemblyInfo - CommonBus - new version: 5.01.06 - ready to generate nuget package.
    Issue #: 4660

--- VS/trunk/CommServer/CommServer.CommonBus/Properties/AssemblyInfo.cs
+++ VS/trunk/CommServer/CommServer.CommonBus/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@
 //  $URL$
 //  $Id$
 //
-//  Copyright (c) 2000-2016 CAS LODZ POLAND
+//  Copyright (c) 2017 CAS LODZ POLAND
 //  +48 (42) 686 25 47
 //  techsupp@cas.eu
 //  www.cas.eu
@@ -21,11 +21,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CAS")]
 [assembly: AssemblyProduct("CAS CommServer Family")]
-[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
+[assembly: AssemblyCopyright("Copyright (c) 2017 CAS LODZ POLAND")]
 [assembly: AssemblyTrademark("CommServer")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8cf11d5b-146c-461d-8e09-c8afeac943f6")]
-[assembly: AssemblyVersion("5.01.06.*")]
-[assembly: AssemblyFileVersion("5.01.06")]
+[assembly: AssemblyVersion("5.01.07.*")]
+[assembly: AssemblyFileVersion("5.01.07")]
 
