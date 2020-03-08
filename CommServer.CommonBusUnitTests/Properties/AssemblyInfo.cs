c8f38763556596963c19a95f975ecb4e10294c10
mpostol <mpostol@cas.eu>
2020-03-08 23:23:18 +0100

    Release 6.0 #13
    - updated version 6.00.03-Charlie
    - ready to be released
    - UT :+1:

--- CommServer.CommonBusUnitTests/Properties/AssemblyInfo.cs
+++ CommServer.CommonBusUnitTests/Properties/AssemblyInfo.cs
@@ -18,6 +18,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("d7e134e8-6bd7-4250-9722-1991ce44cd3d")]
-[assembly: AssemblyVersion("6.00.00.*")]
-[assembly: AssemblyFileVersion("6.00.00")]
+[assembly: AssemblyVersion("6.00.03.*")]
+[assembly: AssemblyFileVersion("6.00.03")]
 
